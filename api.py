ac1164699cc7933483776c2868736ba932a67db9
gabrielbarbosa-physics <gabriel0barbosa0@gmail.com>
2025-12-07 19:11:16 -0300

    Update api.py

--- api.py
+++ api.py
@@ -1,43 +1,20 @@
 from passlib.hash import sha256_crypt
 from flask import Flask, jsonify, render_template, request, session, flash, redirect, url_for
 import psycopg2
-from functools import wraps
 
+# Inicializa o flask e atribui uma chave secreta.
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5fd5f4df5e8f12sf1dv1d5vf'
 
-##############CONFERIR SENHA
 
-
-
-# função para conferir se o usuario esta logado para acessar a dashboard
-
-#def login_required(f):
-#    """
-#    Um decorator para garantir que um usuário esteja logado
-#    antes de acessar uma página.
-#    """
-#    @wraps(f)
-#    def decorated_function(*args, **kwargs):
-#        # Verifica se 'usuario' NÃO está na sessão
-#        if 'usuario' not in session:
-#            # Se não estiver, redireciona para a página de login (index)
-#            return redirect(url_for('index')) # Assumindo que 'index' é sua rota de login
-#
-#        # Se ESTIVER na sessão, execute a função original (o dashboard)
-#        return f(*args, **kwargs)
-#    return decorated_function
-
-
-# dados de conexão no banco local dev
+# Dados de conexão no banco local postgres (atual).
 DB_HOST = "localhost"
 DB_NAME = "volei"
 DB_USER = "postgres"
 DB_PASSWORD = "1728"
 DB_PORT = 5432
 
-# função que cria uma instância de conexão no banco; retorna
-# o objeto "connection"
+# Função que cria uma instância de conexão no banco e retorna o objeto.
 def get_db_connection():
     conn = psycopg2.connect(
         host=DB_HOST,
@@ -48,49 +25,54 @@ def get_db_connection():
     )
     return conn
 
-# carrega a página principal do web app, verificar se o usuario está logado
+# Carrega a página principal do web app, verificar se o usuario está logado.
 @app.route('/')
 def index():
     if 'usuario' in session:
         return render_template('dashboard.html', usuario_logado = session['usuario'])
     return render_template('index.html', usuario_logado = None)
 
+# Se o usuário recarregar a página dashboard sem estar logado, ele retorna para a página index. 
 @app.route('/dashboard')
 def dashboard():
     if 'usuario' in session:
         return render_template('dashboard.html', usuario_logado = session['usuario'])
     return render_template('index.html', usuario_logado= None)
 
+# Método de login.
 @app.route('/login', methods=['GET','POST'])
 def login():
-    # Se for GET, apenas renderiza a página
+    # Se for GET, apenas renderiza a página.
     if request.method == 'GET':
-        # Se já estiver logado, redireciona para o index
+        # Se já estiver logado, redireciona para o index.
         if 'usuario' in session:
-            return redirect(url_for('index'))
-        return render_template('index.html') # Assumindo que o login está no index
+            return redirect(url_for('dashboard'))
+        return render_template('index.html') 
 
-    # Se for POST, processa o login
+    # Se for POST, processa o login.
     conn = None
+    # Cria um objeto request que recebe um JSON enviado para o servidor.
     try:
         data_json = request.json
         usuario = data_json.get('usuario')
         senha = data_json.get('senha')
-
+        # Se o usuário tentar enviar uma requisição com faltas, o servidor responde sem sucesso.
         if not usuario or not senha:
              return jsonify({'sucesso': False, 'erro': 'Usuário e senha são obrigatórios.'})
-        
+        # Cria uma conexão com o banco.
         conn = get_db_connection()
+        
         with conn.cursor() as cursor:
+            # Busca via (SQL query) os dados de login do usuário; caso não encontre nenhum, devolve um erro.
             cursor.execute("SELECT senha_hash FROM atletas WHERE usuario = %s;", (usuario,))
             resultado_query=cursor.fetchone()
             if resultado_query:
                 atleta_senha_hash_memory_view = resultado_query[0]
                 atleta_senha_hash = atleta_senha_hash_memory_view.tobytes()
-            
+                # Compara o hash da senha com o hash do banco.
                 if sha256_crypt.verify(senha, atleta_senha_hash):
                     session['usuario'] = usuario
-                    # 3. MUDANÇA (Importante): Retorna JSON para o JavaScript lidar com o redirecionamento
+                    # Retorna bool(sucesso) como um JSON para o usuário.
                     return jsonify({'sucesso': True, 'redirect_url': url_for('dashboard')})
                 else:
                     return jsonify({'sucesso': False, 'erro': 'Senha incorreta.'})
@@ -102,6 +84,7 @@ def login():
         return jsonify({'sucesso': False, 'erro': f'Ocorreu um erro interno: {e}'})
     
     finally:
+        # Encerra a conexão com o banco.
         if conn:
             conn.close()
 
